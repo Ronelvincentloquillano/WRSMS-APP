590ff5b845459d2eb5cb9778e786d5515af87ed4
gidielgutierrez <gidielg@gmail.com>
2025-12-25 15:29:07 +0800

    UI refactored

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./wrsm_app/templates/**/*.html",
+    "./wrsm_app/templates/*.html",
+    "./static/src/**/*.{css,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
