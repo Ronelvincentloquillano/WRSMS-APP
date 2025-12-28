5cf562bfe05eb279031dc47f80b16d9d44cc9f77
gidielgutierrez <gidielg@gmail.com>
2025-12-28 15:48:55 +0800

    re-arranged navbar layout and fixed minor styling issue

--- wrsm_app/templatetags/custom_tags.py
+++ wrsm_app/templatetags/custom_tags.py
@@ -12,6 +12,10 @@ def remove_none(value):
         return ''
     return value
 
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
 @register.filter
 def integer(value):
     return int(value)
